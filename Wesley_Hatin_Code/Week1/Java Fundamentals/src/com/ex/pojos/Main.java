8d624ddbbed0a080e6dedae2836daf699d575588
WesHatin <wjhatin@gmail.com>
2018-02-28 16:25:55 -0500

    Moved the coding challenges around a bit, added serializable stuff. Pretty sure I get it, but a review sometime would be nice.

--- Wesley_Hatin_Code/Week1/Java Fundamentals/src/com/ex/pojos/Main.java
+++ Wesley_Hatin_Code/Week1/Java Fundamentals/src/com/ex/pojos/Main.java
@@ -1,17 +1,30 @@
 package com.ex.pojos;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		Student a = new Student("\n Genesis", 55);
-		Student b = new Student("\n Rick James", 22);
-		Student c = new Student("\n Beyonce", 35);
-		
+	public static void main(String[] args) {	
 		StudentIO io = new StudentIO();
+		Scanner scan = new Scanner(System.in);
+		Student newStudent = new Student();
 		
-		io.writeStudent(a);
-		io.writeStudent(b);
-		io.writeStudent(c);
+		System.out.println("Hello, new student! Welcome to Revature High!");
+		System.out.println("Before we can get started, I need to know your name.");
+		System.out.println("Please enter your name here: ");
+		newStudent.setName(scan.nextLine());
+		
+		String[] name = newStudent.getName().split(" ");
+		System.out.println("Alright! So, " + name[0] + ", how old are you?");
+		newStudent.setAge(Integer.parseInt(scan.nextLine()));
+		io.writeStudent(newStudent);
+		
+		List<Student> students = io.readStudents();
+		for(Student s : students) {
+			System.out.println(s.getName() + " is " + s.getAge() + " years old.");
+		}
+		scan.close();
 
 	}
 
