9884ce6cd356a8cc685c58c12462b18bb5a1858e
Katelyn Peterson <sunstarunicorn@users.noreply.github.com>
2018-02-28 15:10:37 -0600

    Coding Challenge: Read User Input and add to file.  Also added validation for Student age

--- Katelyn_Peterson_Code/Week1/StudentApp/src/com/ex/run/Main.java
+++ Katelyn_Peterson_Code/Week1/StudentApp/src/com/ex/run/Main.java
@@ -1,6 +1,7 @@
 package com.ex.run;
 
 import java.util.List;
+import java.util.Scanner;
 
 import com.ex.io.StudentIO;
 import com.ex.pojos.Student;
@@ -18,7 +19,10 @@ public class Main {
 		io.writeStudent(b);
 		io.writeStudent(c);*/
 		
+		getNewStudent(io);
+		
 		List<Student> students = io.readStudents();
+		
 		
 		for (Student s : students)
 		{
@@ -26,10 +30,42 @@ public class Main {
 		}
 	}
 	
-	static void getNewStudent()
+	static void getNewStudent(StudentIO io)
 	{
 		// Read in from console
+		Scanner input = new Scanner(System.in);
+		String info;
+		Student temp = new Student();
 		
+		System.out.println("What is your name: ");
+		info = input.nextLine();
+		
+		temp.setName(info);
+		
+		System.out.println("What is your age: ");
+		validateInput(input);
+		info = input.nextLine();
+		
+		temp.setAge(Integer.parseInt(info));
+		
+		//students.add(temp);
+		io.writeStudent(temp);
+		
+		//System.out.println(temp.getName() + " is " + temp.getAge() + " years old.");
+		
+		input.close();
+	}
+	
+	static void validateInput(Scanner input)
+	{
+		System.out.println("Enter age: ");
+		
+		while (!input.hasNextInt())
+		{
+			System.out.println("Please enter an Integer.");
+			System.out.println("Enter age: ");
+			input.nextLine();
+		}
 	}
 
 }
