aefad6386f89dd47ea33fb5778222f514e6a377c
William Rosser <will.e179@gmail.com>
2018-02-28 15:05:52 -0500

    More POJO file IO

--- William_Rosser_Code/Pojos/src/com/ex/io/StudentIO.java
+++ William_Rosser_Code/Pojos/src/com/ex/io/StudentIO.java
@@ -1,6 +1,9 @@
 package com.ex.io;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,12 +20,22 @@ public class StudentIO {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 	
 	public List<Student> readStudents() {
 		List<Student> students = new ArrayList<Student>();
-		
+		try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			String line = null;
+			while((line=br.readLine()) != null) {
+				String[] data = line.split(":");
+				Student temp = new Student(data[0], Integer.parseInt(data[1]));
+				students.add(temp);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
 		return students;
 	}
 }
--- William_Rosser_Code/Pojos/src/com/ex/run/Main.java
+++ William_Rosser_Code/Pojos/src/com/ex/run/Main.java
@@ -1,21 +1,49 @@
 package com.ex.run;
 
+import java.util.List;
+import java.util.Scanner;
+
 import com.ex.io.StudentIO;
 import com.ex.pojos.Student;
 
 public class Main {
 
+	private static StudentIO io;
 	public static void main(String[] args) {
-		Student a = new Student("William Rosser", 21);
-		Student b = new Student("Nick Hale", 22);
-		Student c = new Student("Damien", 666);
-		
-		StudentIO io = new StudentIO();
-		
-		io.writeStudent(a);
-		io.writeStudent(b);
-		io.writeStudent(c);
-
+//		Student a = new Student("William Rosser", 21);
+//		Student b = new Student("Nick Hale", 22);
+//		Student c = new Student("Damien", 666);
+//		
+		io = new StudentIO();
+//		
+//		io.writeStudent(a);
+//		io.writeStudent(b);
+//		io.writeStudent(c);
+		List<Student> students = io.readStudents();
+		for(Student s : students) {
+			System.out.println(s.getName() + " is " + s.getAge() + " years old.");
+		}
+		getNewStudent();
+	}
+	
+	static void getNewStudent() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a new Student's name:");
+		String name = sc.nextLine();
+		Student s = new Student();
+		s.setName(name);
+		int age = -1;
+		while (age < 0) {
+			try {
+				System.out.println("Enter the new student's age:");
+				age = Integer.parseInt(sc.nextLine());
+			} catch (NumberFormatException e) {
+				System.out.println("Enter a real age.");
+			}
+		}
+		s.setAge(age);
+		System.out.println("Added: " + s.toString());
+		io.writeStudent(s);
 	}
 
 }
