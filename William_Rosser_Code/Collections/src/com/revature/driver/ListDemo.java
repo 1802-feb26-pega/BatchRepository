ab724edd5fb2c748e3c585e4ebba9bb5ae799608
William Rosser <will.e179@gmail.com>
2018-03-01 11:22:22 -0500

    Class Notes

---
+++ William_Rosser_Code/Collections/src/com/revature/driver/ListDemo.java
@@ -0,0 +1,29 @@
+package com.revature.driver;
+
+import java.util.*;
+
+public class ListDemo {
+
+	@SuppressWarnings({ "rawtypes", "unchecked"})
+	public static void main(String[] args) {
+		List myList = new ArrayList();
+		myList.add(10);
+		myList.add(1,9);
+		myList.add(2,8);
+		myList.add(7);
+		
+		List myOtherList = new ArrayList();
+		myOtherList.add(2);
+		myOtherList.add(3);
+		myOtherList.add(5);
+		myOtherList.add(7);
+		myOtherList.add(9);
+		myOtherList.add(11);
+		myOtherList.add(13);
+		
+		myList.addAll(1, myOtherList);
+		System.out.println(myList);
+
+	}
+
+}
