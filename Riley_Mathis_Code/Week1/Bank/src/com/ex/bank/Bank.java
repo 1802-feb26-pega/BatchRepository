5eb8a063ecb758813e5076117dfe365db65ea754
Riley Mathis <rileydmathis@gmail.com>
2018-03-04 17:49:02 -0500

    Implement Login method

--- Riley_Mathis_Code/Week1/Bank/src/com/ex/bank/Bank.java
+++ Riley_Mathis_Code/Week1/Bank/src/com/ex/bank/Bank.java
@@ -1,12 +1,16 @@
 package com.ex.bank;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Bank {
 	//static Set<String> users = new HashSet<>();
@@ -41,15 +45,17 @@ public class Bank {
 		un = scan.nextLine();
 		System.out.println("Password:");
 		pw = scan.nextLine();
+		
 		User user = new User(fn,ln,un,pw);
 		File fileName = new File(user.getFileName());
+		
 		if(fileName.exists()) {
 			System.out.println("Username already exists!");
 			register(scan);
 		}
 		else {
 			String output = fn+":"+ln+":"+un+":"+pw+":"+user.getBalance();
-			users.add(un);
+			//users.add(un);
 			try(BufferedWriter bw = new BufferedWriter(new FileWriter(user.getFileName(), true))){
 				bw.write(output.toString());
 			} catch(IOException e) {
@@ -60,8 +66,42 @@ public class Bank {
 	}
 	
 	public static void login(Scanner scan) {
+		String un,pw,checkPw = "";
+		List<String> data = new ArrayList<>();
 		System.out.println("Login");
 		System.out.println("Username:");
+		un = scan.nextLine();
+		File fileName = new File("/src/data/"+un+".txt");
+		System.out.println(un + "  " + fileName);
+		
+		if(fileName.exists()) {
+			try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+				String line = br.readLine();
+				data = Arrays.asList(line.split(":"));
+			} catch(FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			checkPw = data.get(3);
+		}
+		else {
+			System.out.println("That username doesn't exist!");
+			login(scan);
+		}
+		System.out.println("Password:");
+		pw = scan.nextLine();
+		
+		if(pw.equals(checkPw)) {
+			atm(scan);
+		}
+		else {
+			System.out.println("Incorrect password for Username: " + un);
+			login(scan);
+		}
+		
+	}
+	public static void atm(Scanner scan) {
 		
 	}
 }
