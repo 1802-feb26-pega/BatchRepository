64a7d70c25c6c2f78792f4f187abf3f597529238
duyhong <duyhong@berkeley.edu>
2018-03-04 12:07:26 -0500

    Bank Assignment Starter

---
+++ duy_hong_code/week1/BankAssignment/src/com/ex/projects/User.java
@@ -0,0 +1,13 @@
+package com.ex.projects;
+
+public class User {
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	private double balance;
+	
+	public User() {}
+	
+	
+}
