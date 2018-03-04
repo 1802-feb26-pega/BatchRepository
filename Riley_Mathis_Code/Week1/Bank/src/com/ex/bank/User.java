25595e01133202cff03f82be1f7b29f214679e8b
Riley Mathis <rileydmathis@gmail.com>
2018-03-04 15:23:49 -0500

    Begin scaffolding of User Class

--- Riley_Mathis_Code/Week1/Bank/src/com/ex/bank/User.java
+++ Riley_Mathis_Code/Week1/Bank/src/com/ex/bank/User.java
@@ -1,5 +1,74 @@
 package com.ex.bank;
 
-public class User {
+import java.io.*;
 
+public class User {
+	
+	public String firstName;
+	public String lastName;
+	public String username;
+	private String password;
+	private int balance;
+	
+	
+	public User(String firstName, String lastName, String username, String password) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+	}
+	
+	public void login() {
+		
+	}
+	
+	public void register() {
+		
+	}
+	
+	public void logout() {
+		
+	}
+	
+	public void withdraw() {
+		
+	}
+	
+	public void deposit() {
+		
+	}
+	
+	
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
