c7d1075646f1b99fa7ab4c816be9a4fd75d2c5bd
Thomas Hemming <hemming.thomas@gmail.com>
2018-03-12 00:43:41 -0400

    done for real this time?

--- Thomas_Hemming_Code/Week2/SQL Workbook/RevatureAssociateSQLWorkbook_Section6.sql
+++ Thomas_Hemming_Code/Week2/SQL Workbook/RevatureAssociateSQLWorkbook_Section6.sql
@@ -5,3 +5,27 @@ Task - Create an after insert trigger on the employee table fired after a new re
 Task – Create an after update trigger on the album table that fires after a row is inserted in the table
 Task – Create an after delete trigger on the customer table that fires after a row is deleted from the table.
 */
+
+CREATE OR REPLACE TRIGGER after_employee_insert
+AFTER
+INSERT ON employee
+BEGIN
+    dbms_output.put_line('employee added');
+END;
+/
+
+CREATE OR REPLACE TRIGGER after_album_insert
+AFTER
+UPDATE ON album
+BEGIN
+    dbms_output.put_line('album added');
+END;
+/
+
+CREATE OR REPLACE TRIGGER after_customer_delete
+AFTER
+DELETE ON customer
+BEGIN
+    dbms_output.put_line('customer deleted');
+END;
+/
