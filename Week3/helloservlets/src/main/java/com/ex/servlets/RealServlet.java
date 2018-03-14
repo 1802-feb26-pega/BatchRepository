a14660f28457d0eda69c6af908cfcfa83ba42aa3
genesisb17 <genesis.bonds@gmail.com>
2018-03-14 17:30:34 -0400

    adding doget functionality

--- Week3/helloservlets/src/main/java/com/ex/servlets/RealServlet.java
+++ Week3/helloservlets/src/main/java/com/ex/servlets/RealServlet.java
@@ -2,6 +2,7 @@ package com.ex.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/real")
 public class RealServlet extends HttpServlet {
+	//access servlet at [servername]:[port]/[contextroot]/[urlpattern]
+	//context root in maven projects will be artifact id
+	//can check or change it at project properties -> web project settings -> context root
+	//https://learn.onemonth.com/understanding-http-basics/
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -19,10 +24,37 @@ public class RealServlet extends HttpServlet {
 		PrintWriter out = resp.getWriter();
 		resp.setContentType("text/html");
 		out.println("Welcome to your second servlet!<br>");
-		out.println(req.getParameter("name"));
-		System.out.println(req.getParameter("name" ));
+//		out.println(req.getParameter("name"));
+//		System.out.println(req.getParameter("name"));
+		
+		//sample query string:
+		//http://localhost:8099/helloservlets/real?name=genesis&age=100&bio=this+is+my+bio
+		Enumeration<String> paramNames = req.getParameterNames();
+		while(paramNames.hasMoreElements()) {
+			String param = paramNames.nextElement();
+			String val = req.getParameter(param);
+			
+			System.out.println(param + " : " + val);
+			out.println(param + " : " + val + "<br>");
+		}
+		
+		out.println("<hr><br><h3>About our request...</h3><br>" +
+					req.getMethod() + " " + req.getServerName() +":" + req.getServerPort() + "<br>");
+		
+		Enumeration<String> info = req.getHeaderNames();
+		while(info.hasMoreElements()) {
+			String name = info.nextElement();
+			String value = req.getHeader(name);
+			
+			System.out.println(name + " : " + value);
+			out.println(name + " : " + value + "<br>");
+			
+		}
+		
 			
 	}
+	
+	
 	
 	
 
---
+++ Week3/helloservlets/src/main/java/com/ex/servlets/UserServlet.java
@@ -0,0 +1,44 @@
+package com.ex.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ex.pojos.User;
+import com.ex.service.DemoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebServlet("/users")
+public class UserServlet extends HttpServlet{
+	
+	static DemoService service = new DemoService();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		System.out.println("IN DO GETMETHOD IN USER SERVLET");
+		ArrayList<User> users = service.getAll();
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(users.toArray());
+		
+		PrintWriter out = resp.getWriter();
+		resp.setContentType("application/json");
+		out.write(json);
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+
+}
