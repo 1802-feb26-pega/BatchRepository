0f69801d18305644e7c3655b9414397408153d8d
Katelyn Peterson <sunstarunicorn@users.noreply.github.com>
2018-03-14 11:48:12 -0500

    Syncing Git -- AGAIN

--- Katelyn_Peterson_Code/Week3/UnderstandingAJAX/WebContent/js/sw.js
+++ Katelyn_Peterson_Code/Week3/UnderstandingAJAX/WebContent/js/sw.js
@@ -1,4 +1,17 @@
-// Notes - add later
+/*AJAX is useful because it allows developers to:
+	- Update a web page without reloading the page
+	- Request data from a server - after the page has loaded
+	- Receive data from a server - after the page has loaded
+	- Send data to a server in the background 
+About AJAX
+	- Not a programming language, but a technique for accessing web servers from a web page
+	- AJAX stands for Asynchronous JavaScript and XML
+	- Uses a combination of 
+		○ A browser built-in XMLHttpRequest object (to request data from a web server)
+		○ JS and HTML DOM (to display or use the data)
+	- AJAX is a misleading name. AJAX apps might use some XML to transport data but it is equally common to transport data as plain text or JSON text
+	- Allows web pages to be updated asynchronously by exchanging data with a web server behind the scenes. This means that it is possible to update parts of a webpage without reloading the whole page
+*/
 
 window.onload = function()
 {
