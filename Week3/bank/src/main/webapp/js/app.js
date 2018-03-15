cbbd1242a985bd570f354411875802f9bf375963
taikwando <tsavage@udel.edu>
2018-03-15 12:53:47 -0400

    commiting changes to code

--- Week3/bank/src/main/webapp/js/app.js
+++ Week3/bank/src/main/webapp/js/app.js
@@ -10,6 +10,21 @@ function loadLanding(){
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4 && xhr.status == 200){
 			$('#view').html(xhr.responseText);
+			
+			$('#login').on('click',login);
+			// after text is loaded, add event listeners/functionality to view
 		}
 	}
+}
+
+function login() {
+	var email = $('#email').val();
+	var password = $('#pass').val();
+
+	var toSend = [email, password]
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "login", true);
+	xhr.send(JSON.stringify(toSend));
+	
 }
